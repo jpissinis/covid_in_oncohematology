52afed70bfd2ecdbc35e8aac4a4af17914a263e2
jJrónimo Pissinis <jpissinis@gmail.com>
2021-03-02 07:28:52 -0300

    seprating the analysis for the comorbidities and the age

--- scripts/quantitatives.R
+++ scripts/quantitatives.R
@@ -11,25 +11,24 @@ library(writexl)
 path_name<-file.path("./raw","patients.csv")
 patients<-read_csv(path_name)
 
-#selecting the relevant variables for the analysis
-quant_patients<-patients%>%select( 
-  PLASMA, `Infusion temprana 1 tardia 0`,
-  `Edad al momento de la Infeccion por SARS-CoV2`, COMORBILIDADES,
-  EPOC, `Tumor Solido`, `ICC/CI`, Obesidad, HTA, DIABETES,
-  `TABAQ/EXTABAQ`, `Enfermedad Autoinmune`, ERC)
+#selecting the comorbidities variables
+comorbidities_patients<-patients%>%select( 
+  PLASMA, `Infusion temprana 1 tardia 0`,EPOC, `Tumor Solido`,
+  `ICC/CI`, Obesidad, HTA, DIABETES,`TABAQ/EXTABAQ`,
+  `Enfermedad Autoinmune`, ERC)
 
 #defining the events
 events<-c("SI")
 
 #wrangling the columns and translating the events
 is_event<-function(x){ifelse(is.na(x),x,x%in%events)}
-quant_patients<-quant_patients%>%
+comorbitities_patients<-comorbidities_patients%>%
   mutate_if(is.numeric,as.logical)%>%
   mutate_if(is.character,is_event)%>%
   mutate_if(is.character,as.logical)
 
 #counting the comorbidities
-quant_patients<-quant_patients%>%
+comorbidities_patients<-comorbidities_patients%>%
   mutate(N_comorbilidades=
     EPOC+`Tumor Solido`+`ICC/CI`+Obesidad+HTA+DIABETES+
     `TABAQ/EXTABAQ`+`Enfermedad Autoinmune`)%>%
@@ -37,8 +36,19 @@ quant_patients<-quant_patients%>%
            ifelse(is.na(ERC),N_comorbilidades,
                   N_comorbilidades+ERC))
 
-#obtaining the median
-median_quant_patients<-quant_patients%>%summarise_all(median)
+#grouping by the comparison variable
+comorbidities_patients<-comorbidities_patients%>%
+  group_by(PLASMA)
+
+#obtaining the median number of comorbidities
+median_na_rm<-function(x){median(x,na.rm=T)}
+median_comorbidities_patients<-comorbidities_patients%>%
+  select(PLASMA,N_comorbilidades)%>%summarise_all(median_na_rm)
+median_comorbidities_patients
+
+IQR(comorbidities_patients$N_comorbilidades)
+?IQR
+quantile(comorbidities_patients$N_comorbilidades,0.75)
 
 #plotting the number of comorbidities
 quant_patients%>%select(N_comorbilidades,PLASMA)%>%
